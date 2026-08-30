6e18ead0f127b43307fa77e680576f4f8c507a60
Abdul Hannan Zahid <hannanzahid27905@gmail.com>
2026-08-30 23:19:46 +0500

    Adding JS file

---
+++ script.js
@@ -0,0 +1,23 @@
+let productImg = document.querySelector("#productImg");
+let btn = document.querySelectorAll(".btn");
+btn[0].onclick = function () {
+    productImg.src = "assets/images/image1.png";
+    for (bt of btn) {
+        bt.classList.remove("active");
+    }
+    this.classList.add("active");
+}
+btn[1].onclick = function () {
+    productImg.src = "assets/images/image2.png";
+    for (bt of btn) {
+        bt.classList.remove("active");
+    }
+    this.classList.add("active");
+}
+btn[2].onclick = function () {
+    productImg.src = "assets/images/image3.png";
+    for (bt of btn) {
+        bt.classList.remove("active");
+    }
+    this.classList.add("active");
+}
